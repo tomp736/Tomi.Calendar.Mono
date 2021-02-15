985fcd77232e097acab524498397c3fea03f3149
Thomas Pisula <tompisula@gmail.com>
2021-02-15 14:56:12 +0100

    create generic list component

---
+++ Client/Components/_/GenericList.cs
@@ -0,0 +1,15 @@
+﻿using Fluxor.Blazor.Web.Components;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace Tomi.Calendar.Mono.Client.Components._
+{
+    public partial class GenericList<ItemType> : FluxorComponent
+    {
+        [Parameter]
+        public IEnumerable<ItemType> Items { get; set; }
+
+        [Parameter]
+        public RenderFragment<ItemType> ItemFormat { get; set; }
+    }
+}
